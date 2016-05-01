5a7eb1cf5fa64658ecae03690013bcb34be6644d
Nikolay <n.poymanov@gmail.com>
2016-05-01 06:55:05 +0300

    [new] add new avatar upload
    Реализована загрузка нового аватара пользователя

--- js/script.js
+++ js/script.js
@@ -59,6 +59,18 @@ var newAvatar = document.querySelector('.new-avatar');
 // Кнопка закрытия форма изменения аватара
 var newAvatarClose = document.querySelector('.new-avatar__form-close');
 
+// Зона для загрузки файла
+var newAvatarUpload = document.querySelector('.new-avatar__upload');
+
+// Область вывода ошибки в форме загрузки аватара
+var newAvatarError = document.querySelector('.new-avatar__error');
+
+// Файл с изображением нового аватара
+var newAvatarFile = "";
+
+// Кнопка загрузка изображения
+var newAvatarSubmit = document.querySelector('.new-avatar__form-submit');
+
 // Подключение handlebars helper
 Handlebars.registerHelper('formatDate', function(ts) {
 	return new Date(ts).toLocaleString();
@@ -108,6 +120,9 @@ newAvatarLink.addEventListener('click',function(e){
 	// Отображаем форму изменения аватара
 	newAvatar.style.display = "block";
 
+	// Сбрасываем область просмотра изображения
+	newAvatarUpload.innerHTML = "Загрузить сюда фото";
+
 });
 
 // Закрытие формы изменения аватара
@@ -121,6 +136,101 @@ newAvatarClose.addEventListener('click',function(e){
 	// Скрываем форму изменения аватара
 	newAvatar.style.display = "none";
 
+});
+
+// События для формы загрузки нового аватара
+
+newAvatarUpload.addEventListener('dragover', function(e){
+	e.stopPropagation();
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'copy';
+}, false);
+
+
+newAvatarUpload.addEventListener('drop', function(e){
+	e.stopPropagation();
+  e.preventDefault();
+
+  var file = e.dataTransfer.files[0];
+
+  // Прерываем функцию, если возникает какая-то ошибка чтения файла
+  if(!file){
+  	return;
+  }
+
+  // Скрываем блок с описанием ошибки
+  newAvatarError.style.display = "none";
+  newAvatarError.innerHTML = "none";
+
+  // Ошибка: неверный тип файла
+  if(file['type'] != 'image/jpeg') {
+  	newAvatarError.style.display = "block";
+  	newAvatarError.innerHTML = "Неверный формат файла! Разрешен только JPEG!";
+  	return;
+  }
+
+  filesize = file['size'] / 1024;
+
+  // Ошибка: не более 512 кб
+  if(filesize > 512) {
+  	newAvatarError.style.display = "block";
+  	newAvatarError.innerHTML = "Превышение размера файла! Допустимый размер 512 кб!";
+  	return;
+  }
+
+  // Отображаем превью изображения
+  var fileReader = new FileReader();
+
+  fileReader.readAsDataURL(file);
+
+  fileReader.addEventListener('load',function(){
+
+  	// Убираем текст-подсказку
+  	newAvatarUpload.innerHTML = "";
+
+  	// Создаем изображение
+
+  	var previewImage = document.createElement('img');
+  	previewImage.classList.add('new-avatar__upload-preview');
+		previewImage.src = this.result;
+
+		// Выводим изображение
+		newAvatarUpload.appendChild(previewImage);
+
+		// Сохраняем файл в переменную
+		var newAvatarFile = file;
+
+  });
+
+}, false);
+
+// Загрузка нового аватара
+newAvatarSubmit.addEventListener('click',function(e){
+		e.preventDefault();
+
+		// Отправляем изображение на сервер
+		var newImage = new FormData();
+		newImage.append('photo',newAvatarFile);
+		newImage.append('token',userToken);
+
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', 'http://localhost:5000/upload', false);
+		xhr.send(newImage);
+
+		if (xhr.status != 200) {
+  		// Если не удалось загрузить фотографию
+  		newAvatarError.style.display = "block";
+  		newAvatarError.innerHTML = "Ошибка загрузки фотографии";
+		} else {
+			// Если загрузка прошла успешно
+
+			// Закрываем форму
+			e.preventDefault();
+			// Скрываем блок блокиратор
+			blockerBlock.style.display = "none";
+			// Скрываем форму изменения аватара
+			newAvatar.style.display = "none";
+		}
 });
 
 // Кнопка отправить сообщение
